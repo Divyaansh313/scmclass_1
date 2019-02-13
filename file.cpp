cec13ba1e8ed3b64c56e129db5bb99cc97c4d39b
Divyaansh Jain <divyaansh03anu@gmail.com>
2019-02-13 14:56:44 +0530

    simple sum operation

--- file.cpp
+++ file.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
+using namespace std;
 int main()
 {
 	cout<<"Hello world"<<endl; //printed message//
 	cout<<"Today is Wednesday"<<endl; //changed printed message//
 
-	return0;
+	return 0;
 
 }   
---
+++ file_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a = 1;
+	int b = 2;
+	int sum;
+	sum = a+b;
+
+	cout<<"The sum is"<<sum<<endl; //sum operation//
+	return 0;
+}
