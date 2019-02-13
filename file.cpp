37d52b3ccbdcea82d87cab36133385a4faa980b0
Divyaansh Jain <divyaansh03anu@gmail.com>
2019-02-13 14:48:55 +0530

    changed printed message

--- file.cpp
+++ file.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 int main()
 {
-	cout<<"Hello world"<<endl;
+	cout<<"Hello world"<<endl; //printed message//
+	cout<<"Today is Wednesday"<<endl; //changed printed message//
+
 	return0;
 
-}
+}   
